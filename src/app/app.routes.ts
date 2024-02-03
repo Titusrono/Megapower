9220688c008d6c52a53133a75ac7a79bb28cb4c5
Titusrono <tituskrono04@gmail.com>
2024-02-03 09:10:49 +0300

    megapower project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { TeamComponent } from './team/team.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': 'home', component:HomeComponent},
+    {'path': 'product', component:ProductComponent},
+    {'path': 'team', component:TeamComponent},
+    {'path': 'user', component:UserComponent}
+];

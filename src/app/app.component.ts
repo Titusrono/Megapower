2d81fd6db322ff2a23a4ac911001f5d63c6c8c8e
Titusrono <tituskrono04@gmail.com>
2024-02-07 16:43:06 +0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,NavbarComponent, RouterOutlet],
+  imports: [CommonModule,NavbarComponent,FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
